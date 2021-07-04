f162ba59ef81549f70c08fea9a8efafa2c2458e9
konstantin <chernyakonstantin@gmail.com>
2021-07-04 19:01:51 +0700

    Рефакторинг классов

--- obstacle.py
+++ obstacle.py
@@ -1,47 +1,82 @@
+from typing import Any, Tuple
+
 import pygame
 
-class Line:  #Класс линий, из которых состоит препятствие
-    def __init__(self, x1, y1, x2, y2): 
-        self.a = x1,y1
-        self.b = x2,y2
-        
-    def shifted_copy(self, shift):
-        a = self.a[0] + shift, self.a[1]
-        b = self.b[0] + shift, self.b[1]
-        return Line(*a,*b)
-        
-    def show(self, surface):
-        pygame.draw.line(surface, 
-                         (255,255,255), #White color
-                         self.a,
-                         self.b)
+from abstract_classes import GameObject
 
-class Obstacle: #Класс препятствия
-    def __init__(self, coord, size):
-        self.coord = coord #x,y
-        self.size = size #width, height
-        self.edges = []
-        self.make_object()
-        
-    def make_object(self): #Создает прямоугольный объект
-        self.edges.append(Line(self.coord[0], 
-                          self.coord[1], 
-                          self.coord[0] + self.size[0], 
-                          self.coord[1]))
-        self.edges.append(Line(self.coord[0] + self.size[0], 
-                          self.coord[1], 
-                          self.coord[0] + self.size[0], 
-                          self.coord[1] + self.size[1]))
-        self.edges.append(Line(self.coord[0], 
-                          self.coord[1] + self.size[1], 
-                          self.coord[0] + self.size[0], 
-                          self.coord[1] + self.size[1]))
-        self.edges.append(Line(self.coord[0], 
-                          self.coord[1], 
-                          self.coord[0], 
-                          self.coord[1] + self.size[1]))
-    
+
+class Point:
+    @property
+    def x(self) -> Any:
+        return self._x
+
+    @x.setter
+    def x(self, value: Any) -> None:
+        self._x = value
+
+    @property
+    def y(self) -> Any:
+        return self._y
+
+    @y.setter
+    def y(self, value: Any) -> None:
+        self._y = value
+
+    def __init__(self, x: any, y: Any) -> None:
+        self._x = x
+        self._y = y
+
+
+class Line(GameObject):
+    WHITE = (255, 255, 255)
+    """ Класс линий, из которых состоит препятствие."""
+
+    def __init__(self, x1, y1, x2, y2):
+        self._a = Point(x1, y1)
+        self._b = Point(x2, y2)
+
+    def shifted_copy(self, shift: Any) -> Any:
+        a = self._a.x + shift, self._a.y
+        b = self._b.x + shift, self._b.y
+        return Line(*a, *b)
+
     def show(self, surface):
-        for edge in self.edges:
+        pygame.draw.line(surface,
+                         Line.WHITE,
+                         (self._a.x, self._a.y),
+                         (self._b.x, self._b.y))
+
+    def get_coord(self) -> Tuple:
+        return self._a.x, self._a.y, self._b.x, self._b.y
+
+
+class Obstacle(GameObject):
+    def __init__(self, x: Any, y: Any, width: Any, height: Any):
+        self._x = x
+        self._y = y
+        self._width = width
+        self._height = height
+        self._edges = []
+        self._make_object()
+
+    def _make_object(self):  # Создает прямоугольный объект
+        self._edges.append(Line(self._x,
+                                self._y,
+                                self._x + self._width,
+                                self._y))
+        self._edges.append(Line(self._x + self._width,
+                                self._y,
+                                self._x + self._width,
+                                self._y + self._height))
+        self._edges.append(Line(self._x,
+                                self._y + self._height,
+                                self._x + self._width,
+                                self._y + self._height))
+        self._edges.append(Line(self._x,
+                                self._y,
+                                self._x,
+                                self._y + self._height))
+
+    def show(self, surface):
+        for edge in self._edges:
             edge.show(surface)
-            
