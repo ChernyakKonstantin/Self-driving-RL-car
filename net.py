ce43744335f8122c18e83952c9103290e407292e
konstantin <chernyakonstantin@gmail.com>
2021-07-17 20:49:07 +0700

    Переписать сеть с использованием Keras

--- net.py
+++ net.py
@@ -1,14 +1,12 @@
-import torch.nn as nn
-class myDQN(nn.Module):
-    def __init__(self, n_inputs, hidden_size, n_outputs):
-        super(myDQN, self).__init__()
-        self.net = nn.Sequential(
-            nn.Linear(n_inputs, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, n_outputs),
-        )
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import LSTM, Dense, InputLayer
 
-    def forward(self, x):
-        return self.net(x) #Сеть возвращает Q(s,a) для данного state и всех возможных action
+
+def make_lstm_qdn(n_inputs: int, n_steps: int, n_outputs: int, n_hidden_units: int) -> keras.Model:
+    model = Sequential()
+    model.add(InputLayer(input_shape=(n_steps, n_inputs)))
+    model.add(LSTM(units=n_hidden_units))
+    model.add(Dense(units=n_outputs))
+    model.compile('adam', 'mse')
+    return model
