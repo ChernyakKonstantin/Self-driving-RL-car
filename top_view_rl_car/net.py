91d86b0abea3a332a9617ac3c737b6bf222da34c
konstantin <chernyakonstantin@gmail.com>
2021-07-18 09:17:00 +0700

    Добавить конфигурацию сессии Tensorflow

--- top_view_rl_car/net.py
+++ top_view_rl_car/net.py
@@ -1,6 +1,10 @@
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import LSTM, Dense, InputLayer
+
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 def make_lstm_qdn(n_inputs: int, n_steps: int, n_outputs: int, n_hidden_units: int) -> keras.Model:
