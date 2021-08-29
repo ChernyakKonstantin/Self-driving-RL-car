93f2e699e3c1f3e4f0e29c776a2d04033882c58a
KonstantinChernyak <chernyakonstantin@gmail.com>
2021-08-29 12:43:45 +0300

    Фикс epsilon-планировщика

--- top_view_rl_car/scheduler.py
+++ top_view_rl_car/scheduler.py
@@ -1,14 +1,15 @@
 class EpsilonScheduler:
     def __init__(self, n_steps: int, start_value: float = 1.0, end_value: float = 0.05):
-        self._start_value = start_value
+        self._value = start_value
         self._end_value = end_value
         self._frame_index = 0
         self._n_steps = n_steps
-        self._step = (self._start_value - self._end_value) / self._n_steps
+        self._step = (self._value - self._end_value) / self._n_steps
 
     def get_epsilon(self):
         if self._frame_index <= self._n_steps:
-            epsilon = self._start_value - self._step
+            self._value -= self._step
+            epsilon = self._value
         else:
             epsilon = self._end_value
         self._frame_index += 1
