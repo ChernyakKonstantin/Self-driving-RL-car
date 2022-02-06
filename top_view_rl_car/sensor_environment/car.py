cf5607c3d4c4189648a7a64af8b622ea481b3ce8
KonstantinChernyak <chernyakonstantin@gmail.com>
2022-02-06 22:00:29 +0400

    Simplify "Car" class

--- top_view_rl_car/sensor_environment/car.py
+++ top_view_rl_car/sensor_environment/car.py
@@ -1,4 +1,4 @@
-import math
+from typing import Union, Tuple
 
 import pygame.draw
 
@@ -7,63 +7,77 @@ from .sensor import Sensor
 
 
 class Car(Observable, GameObject, Resetable):
-    INIT_ORIENTATION = 0  # Градусы. Автомобиль перпендикулярен оси X
+    """
+    The car have a sensor to obtain observations.
+    The car is driven forward automatically. The only control is steering.
+    The car keeps the last steering value and accepts change of the value.
+    """
 
-    MIN_STEERING_ANGLE = -45.0
-    MAX_STEERING_ANGLE = 45.0
-    MIN_SPEED = -2.0
-    MAX_SPEED = 2.0
-    SIZE = 10
-
-    @property
-    def x(self):
-        return self._x
-
-    @property
-    def y(self):
-        return self._y
-
-    def __init__(self, x, y):
+    def __init__(self,
+                 x: Union[float, int],
+                 y: Union[float, int],
+                 orientation: float,
+                 velocity: float = 2.0,
+                 steering_bounds: Tuple[float, float] = (-45.0, 45.0),
+                 sprite_size: float = 10,
+                 n_rays: int = 30,
+                 angle_of_view: int = 120,
+                 ray_length: float = 200):
         super().__init__()
-        self._init_x = x
-        self._init_y = y
-        self._x = self._init_x
-        self._y = self._init_y
-        self._orientation = math.radians(Car.INIT_ORIENTATION)
-        self._velocity = 0
-
-        self.sensor = Sensor(self._x, self._y, self._orientation)
+        self._position = pygame.Vector2(x, y)
+        self._orientation = orientation
+        self._velocity = velocity
+        self._steering_bounds = {"min": steering_bounds[0], "max": steering_bounds[1]}
+        self._sprite_size = sprite_size
+        self.sensor = Sensor(self._position.x,
+                             self._position.y,
+                             self._orientation,
+                             n_rays,
+                             angle_of_view,
+                             ray_length)
         self.attach(self.sensor)
 
     def _notify(self):
         for observer in self._observers:
-            observer.update(self._x, self._y, self._orientation)
+            observer.update(self._position.x,
+                            self._position.y,
+                            self._orientation)
 
-    def move(self, steering_angle: float, velocity: float) -> None:
-        self._update_velocity(velocity)
-        self._steer(steering_angle)
-        self._update_coordinates()
+    def move(self, steering_delta: float) -> None:
+        """
+        The method updates position of the car and its observers.
+        Args:
+            steering_delta: Delta of steering.
+        """
+        self._steer(steering_delta)
         self._notify()
 
-    def _update_velocity(self, velocity: float) -> None:
-        self._velocity = velocity
+    def _steer(self, steering_delta: float) -> None:
+        """
+        The method updates the car orientation with checking of bounds.
+        Args:
+            steering_delta: Delta of steering.
+        """
+        orientation = self._orientation + steering_delta
+        if orientation > self._steering_bounds["max"]:
+            self._orientation = self._steering_bounds["max"]
+        elif orientation < self._steering_bounds["min"]:
+            self._orientation = self._steering_bounds["min"]
+        else:
+            self._orientation = orientation
 
-    def _steer(self, cur_angle: float) -> None:
-        self._orientation = math.radians(cur_angle)
-
-    def _update_coordinates(self) -> None:
-        # Автомобиль движется сверху вниз
-        self._y += self._velocity * math.cos(self._orientation)
-        self._x += self._velocity * math.sin(self._orientation)
-
-    def reset(self):
-        self._x = self._init_x
-        self._y = self._init_y
-        self._orientation = math.radians(Car.INIT_ORIENTATION)
-        self._velocity = 0
+    def reset(self,
+              x: Union[float, int],
+              y: Union[float, int],
+              orientation: float):
+        self._position = pygame.Vector2(x, y)
+        self._orientation = orientation
         self._notify()
 
     def show(self, surface) -> None:
-        pygame.draw.circle(surface, pygame.Color('purple'), (self._x, self._y), Car.SIZE, 0)
         self.sensor.show(surface)
-
+        pygame.draw.circle(surface,
+                           pygame.Color('purple'),
+                           (self._position.x, self._position.y),
+                           self._sprite_size,
+                           width=0)
