1caade5e769731fc5f2b92baa86c5719f3c760cb
KonstantinChernyak <chernyakonstantin@gmail.com>
2022-02-06 21:31:32 +0400

    Refactor folder structure

--- top_view_rl_car/__init__.py
+++ top_view_rl_car/__init__.py
@@ -1,7 +1,2 @@
-from .agent import Agent
-from .experience_buffer import ExperienceBuffer
-from .net import make_lstm_qdn
 from .play import play
-from .scheduler import EpsilonScheduler
-from .training import Training
 from .sensor_environment import Environment
