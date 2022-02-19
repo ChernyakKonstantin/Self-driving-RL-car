c2aedf063afc37f85a0a333377adb71191fd6a88
KonstantinChernyak <chernyakonstantin@gmail.com>
2022-02-19 22:40:01 +0300

    Update Environment class

--- top_view_rl_car/sensor_environment/environment.py
+++ top_view_rl_car/sensor_environment/environment.py
@@ -1,124 +1,159 @@
-from typing import Any, List, Tuple
+import sys
+from typing import Any, List, Tuple, Union, Dict
 
+import gym
 import numpy as np
 import pygame
-from gym import core, spaces
 
-from .action_detector import ActionDetector
 from .car import Car
-from .colision_detector import ColisionDetector
+from .collision_detector import CollisionDetector
 from .world import World
 
-import time
 
-class Environment(core.Env):
+class SensorEnvironment(gym.Env):
     """
     Мир со случайно генерируемой дорогой.
 
     Действия:
     * Изменить угол поворота руля: float
-    * Изменить скорость движения: float
 
     Наблюдения:
-    * Текущий угол поворота руля (градусы): float
-    * Текущая скорость: float
     * Данные с датчика расстояния: np.ndarray of float
+    * Текущая ориентация в мире
     """
 
-    # Размеры мира
-    _WORLD_LENGTH = 650
-    _WORLD_WIDTH = 300
-    _SURFACE_SIZE = _WORLD_WIDTH + 50, _WORLD_LENGTH + 50
-    # Начальное положение объекта управления
-    _INIT_CAR_X = _WORLD_WIDTH / 2
-    _INIT_CAR_Y = 30
-    # Параметры трассы
-    _ROAD_WIDTH = 100
-    _START_WALL_X = int(round(_INIT_CAR_X - _ROAD_WIDTH / 2))
-    _START_WALL_Y = 5
-
-    # Параметры завершения эпизода
-    _NO_ACTION_LIMIT = 10
-    _MIN_DISTANCE_TO_OBSTACLE = 1
     # Параметры награды
-    LOOSE_REWARD = -1
-
     metadata = {'render.modes': ['human', ]}
 
-    action_space = spaces.Dict({
-        'steering': spaces.Box(low=Car.MIN_STEERING_ANGLE, high=Car.MAX_STEERING_ANGLE, shape=(1,), dtype=np.float32),
-        'velocity': spaces.Box(low=Car.MIN_SPEED, high=Car.MAX_SPEED, shape=(1,), dtype=np.float32),
-    })
+    def __init__(self, config):
 
-    observation_space = spaces.Dict({
-        'steering': spaces.Box(low=Car.MIN_STEERING_ANGLE, high=Car.MAX_STEERING_ANGLE, shape=(), dtype=np.float32),
-        'velocity': spaces.Box(low=Car.MIN_SPEED, high=Car.MAX_SPEED, shape=(), dtype=np.float32),
-        'distance': spaces.Box(low=0.0, high=200.0, shape=(30, 1), dtype=np.float32),
-    })
+        self._world = World(*config["window_size"],
+                            40,
+                            config["velocity"],
+                            config["part_length"],
+                            config["road_width"])
 
-    def __init__(self):
-        self._world = World(Environment._START_WALL_X, Environment._START_WALL_Y, Environment._WORLD_LENGTH)
-        self._car = Car(Environment._INIT_CAR_X, Environment._INIT_CAR_Y)
-        self._collision_detector = ColisionDetector((self._car.x, self._car.y),
-                                                    self._car.SIZE,
-                                                    Environment._MIN_DISTANCE_TO_OBSTACLE)
-        self._action_detector = ActionDetector(limit=Environment._NO_ACTION_LIMIT)
-        self._car.attach(self._collision_detector)
-        self._car.attach(self._action_detector)
-        pygame.init()
-        self._surface = pygame.display.set_mode(Environment._SURFACE_SIZE)
-        self._next_observation = None
+        init_x = config["window_size"][0] // 2
+        init_y = config["window_size"][1] // 2
+        orientation = 0  # Parallel to Y axis
 
-    # _____Private_methods_____
+        self._car = Car(init_x, init_y,
+                        orientation,
+                        config["velocity"],
+                        config["steering_bounds"],
+                        config["sprite_size"],
+                        config["n_rays"],
+                        config["angle_of_view"],
+                        config["ray_length"],
+                        )
+        self._collision_detector = CollisionDetector(config["sprite_size"])
 
-    def _get_observation(self) -> list:
-        filtered_walls = self._car.sensor.filter_obstacles(self._world.walls)
-        return self._car.sensor.get_view(filtered_walls)
+        self._setup_spaces(config)
+        self._render = config["render"]
+
+        self.state = None
+        self.is_done = None
+        self.counter = None
+        self._car_orientation = None
+        self._intermediate_reward = config["intermediate_reward"]
+        self._loose_reward = config["loose_reward"]
+
+        if self._render:
+            pygame.init()
+            self._surface = pygame.display.set_mode(config["window_size"])
+        else:
+            self._surface = None
+
+    def _setup_spaces(self, config):
+        obs_spaces = gym.spaces.Dict({
+            "sensor_data": gym.spaces.Box(low=0.0,
+                                          high=config["ray_length"],
+                                          shape=(config["n_rays"],),
+                                          dtype=np.float32),
+            "orientation": gym.spaces.Box(low=-180,
+                                          high=180,
+                                          shape=(1,),
+                                          dtype=np.float32),
+        })
+        self.observation_space = obs_spaces
+
+        # Delta of angle to steer the wheel
+        self.action_space = gym.spaces.Box(*config["steering_bounds"],
+                                           shape=(1,),
+                                           dtype=np.float32)
+
+    def _update_orientation(self, angle_delta: Union[int, float]) -> None:
+        orientation = self._car_orientation + angle_delta
+        if orientation > 180:
+            orientation = orientation - 360
+        elif orientation < -180:
+            orientation = orientation + 360
+        self._car_orientation = orientation
+
+    def _filter_non_visible(self) -> np.ndarray:
+        condition_1 = self._world.obstacle_coordinates[3] < self._car._position.y
+        condition_2 = self._world.obstacle_coordinates[1] > self._car._position.y + self._car.sensor._ray_length
+        return np.invert(condition_1 | condition_2)
+
+    def _get_observation(self) -> Dict:
+        mask = self._filter_non_visible()
+        sensor_data = self._car.sensor.get_view(self._world.obstacle_coordinates[:, mask])
+        return {"sensor_data": sensor_data, "orientation": np.array([self._car_orientation])}
 
     def _get_reward(self) -> float:
-        return 0
+        return 1
 
-    # _____Public_methods_____
-
-    def reset(self) -> list:
+    def reset(self) -> Dict:
         self._world.reset()
-        self._collision_detector.reset(*self._world.to_points_array())
-        self._action_detector.reset()
-        self._car.reset()
-
-        self._next_observation = self._get_observation()
-        return self._next_observation
+        self._car.reset(self._world._world_width // 2, self._world._world_length // 2, 0)
+        self.counter = 0
+        self._car_orientation = self._car._orientation
+        self.is_done = False
+        self.state = self._get_observation()
+        return self.state
 
     def is_closed(self) -> bool:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return True
+        if self._render:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return True
 
-    def step(self, action: Tuple[float, float]) -> Tuple[list, float, bool, Any]:
+    def step(self, action: np.ndarray) -> Tuple[Dict, float, bool, Any]:
         """
-        Action хранит в себе текущий угол поворота колес и скорость.
-        TODO: Изменить на дельту.
+        The method change the car orientation with "action" value that is change of the steering angle
+        Args: Change of the car steering angle.
+
+        Returns: Observation, reward, episode completion flag, additional information.
         """
-        self._observation = self._next_observation
-        is_done = self._collision_detector.check() # or self._action_detector()
-        if not is_done:
-            steering_angle, velocity = action
-            self._car.move(steering_angle, velocity)
-            reward = self._get_reward()
+        self._world.translate(action.item(), self._car._position)
+        self._update_orientation(action.item())
+        self.state = self._get_observation()
+        self.is_done = self._collision_detector.check(self.state["sensor_data"])
+
+        if self.is_done:
+            reward = self._loose_reward
         else:
-            reward = Environment.LOOSE_REWARD
-        self._next_observation = self._get_observation()
-        # Дополнительная информация отсутсвует, но она необходима для интерфейса gym
-        info = None
-        return self._next_observation, reward, is_done, info
+            reward = self._intermediate_reward
+
+        # Currently there is no additional info, required by Gym.
+        info = {}
+        self.counter += 1
+        if self._render:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+            # Without the call the windows freezes when no focus.
+            pygame.event.pump()
+            self.render()
+        return self.state, reward, self.is_done, info
 
     def render(self, mode='human') -> None:
         """Параметр mode не используется, но необходим для интерфейса gym."""
-        self._surface.fill(pygame.Color("black"))
-        self._world.show(self._surface)
-        self._collision_detector.show(self._surface)
-        self._car.show(self._surface)
-        pygame.display.flip()
+        if self._render:
+            self._surface.fill(pygame.Color("black"))
+            self._world.show(self._surface)
+            self._car.show(self._surface)
+            pygame.display.flip()
 
     def close(self) -> None:
         pygame.quit()
