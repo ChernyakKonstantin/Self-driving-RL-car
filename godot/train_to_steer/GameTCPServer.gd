db85df70eb0eabaa71f00caa4119e3ee8e211b02
KonstantinChernyak <chernyakonstantin@gmail.com>
2023-06-12 22:19:02 +0300

    Update agent functinality

--- godot/train_to_steer/GameTCPServer.gd
+++ godot/train_to_steer/GameTCPServer.gd
@@ -1,7 +1,8 @@
 extends Node
 # --------
-export(String) var address = "127.0.0.1"
-export(int) var port = 9090
+export var address: String = "127.0.0.1"
+export var port: int = 9090
+export var agent_path: NodePath
 # --------
 enum {
 	REQUEST_FRAME = 1, 
@@ -9,48 +10,78 @@ enum {
 	REQUEST_DISTANCE = 3,  # TODO: currently I think it will be distance the closest object;
 	REQUEST_IS_CRASHED = 4, 
 	REQUEST_WHEEL_POSITION = 5, 
+	REQUEST_SPEED = 6, 
 }
 # --------
+const FRAMES_PER_STEP = 4
+
+const STATUS_KEY = "status"
+const CONFIG_KEY = "config"
+const RESET_KEY = "reset"
+const ACTION_KEY = "action"
+const OBSERVATION_KEY = "observation"
+# --------
 var counter: int = 0  # TODO: Probably delete
+#var frame_counter: int = 0
+#var active_physics_server: bool = false
 # --------
 onready var image_capture = $ImageCapture
 onready var server = TCP_Server.new()
-onready var player = $"../Player"
+onready var agent = get_node(agent_path)
 # --------
 func _ready():
+	print(agent_path)
+#	PhysicsServer.set_active(false)
 	server.listen(port, address)
-	
 
 func _process(delta):
 	if server.is_connection_available():
 		var connection: StreamPeerTCP = server.take_connection()
 		handle_connection(connection)
-
+		
+#func _physics_process(delta):
+#	if Input.is_action_just_pressed("move_forward"):
+#		PhysicsServer.set_active(true)
+#		active_physics_server = true
+#
+#	if active_physics_server == true:
+#		counter += 1
+#	if counter > FRAMES_PER_STEP:
+#		PhysicsServer.set_active(false)
+#		active_physics_server = false
+#		counter = 0
 # --------
 func handle_connection(connection: StreamPeerTCP):
 	var request_package_size = connection.get_available_bytes()
 	print("Request size is: ", request_package_size)
 	var request_data = connection.get_utf8_string(request_package_size)
 	var request = JSON.parse(request_data).result
-
-	if request.has("action"):
-		perform_action(request["action"])
-	
 	var response: Dictionary = {}
 	
-	if request.has("observation"):
-		var observation_requests = request["observation"]
-		if REQUEST_FRAME in observation_requests:
-			response[REQUEST_FRAME] = get_frame()
-		if REQUEST_DEPTH_MAP in observation_requests:
-			response[REQUEST_DEPTH_MAP] = get_depth_map()
-		if REQUEST_DISTANCE in observation_requests:
-			response[REQUEST_DISTANCE] = get_distance()
-		if REQUEST_IS_CRASHED in observation_requests:
-			response[REQUEST_IS_CRASHED] = get_is_crashed()
-		if REQUEST_WHEEL_POSITION in observation_requests:
-			response[REQUEST_WHEEL_POSITION] = get_wheel_position()
+	if request.has(STATUS_KEY):
+		print("Status is requested")
+		response[STATUS_KEY] = 1
 	
+	if request.has(ACTION_KEY):
+		print("Action is requested")
+		agent.perform_action(request[ACTION_KEY])
+	
+	if request.has(OBSERVATION_KEY):
+		var observation_request = request[OBSERVATION_KEY]
+		if REQUEST_FRAME in observation_request:
+			response[REQUEST_FRAME] = get_frame()
+		if REQUEST_DEPTH_MAP in observation_request:
+			response[REQUEST_DEPTH_MAP] = get_depth_map()
+		if REQUEST_DISTANCE in observation_request:
+			response[REQUEST_DISTANCE] = agent.get_proxemity()
+		if REQUEST_IS_CRASHED in observation_request:
+			response[REQUEST_IS_CRASHED] = agent.get_is_crashed()
+		if REQUEST_WHEEL_POSITION in observation_request:
+			response[REQUEST_WHEEL_POSITION] = agent.get_wheel_position()
+		if REQUEST_SPEED in observation_request:
+			response[REQUEST_WHEEL_POSITION] = agent.get_speed()
+	
+	print(response)
 	var json_str = JSON.print(response)
 #	var response_package_size = json_str.length()
 #	print("Response size is: ", request_package_size)
@@ -67,16 +98,9 @@ func get_frame():
 #	# TODO: I need to capture N frames
 #	var frame_data: PoolByteArray = image_capture.get_current_frame_image_as_bytes()
 #	# print(len(frame_data))
-
-	
 func get_depth_map():
 	# TODO: implement in future
 	return [2, ]
-func get_distance():
-	return [3, ]
-func get_is_crashed():
-	return [4, ]
-func get_wheel_position():
-	return [5, ]	
+
 
 
--- godot/train_to_steer/Player.gd
+++ godot/train_to_steer/Player.gd
@@ -1,21 +1,28 @@
 extends VehicleBody
 
-## How fast the player moves in meters per second.
-#export var speed = 14
-## The downward acceleration when in the air, in meters per second squared.
-#export var fall_acceleration = 75
-#
-#var velocity = Vector3.ZERO
-#
-#var _x: int = 0
-#var _z: int = 0
+export var camera_storage_path: NodePath
 
+# Flag whether the agent has colided
+onready var is_collided = false 
+# Flag whether to compute the agent movements
+onready var enabled: bool = false
+# Distance sensors proxemity
+onready var max_distance_sensor_proxemity: float = 1.0  # TODO: make configurable
+
+onready var DistanceSensors = $DistanceSensors
+onready var CameraPlaceholders = $CameraPlaceholders
+# --------
 # Maximum wheel rotational speed in revolutions per minute
 var max_rpm = 500
 var max_torque = 200
-
-
+# --------
+func _ready():
+	_configure_collision()
+	_configure_distance_sensors()
+	_configure_cameras()
+	
 func _physics_process(delta):
+
 	# I think 0.4 is cosine value of angle the wheel turn at
 	steering = lerp(steering, Input.get_axis("move_right", "move_left") * 0.8, 5*delta)
 	var acceleration = Input.get_axis("move_backward", "move_forward")
@@ -25,27 +32,48 @@ func _physics_process(delta):
 	rpm = $back_right_wheel.get_rpm()
 	$back_right_wheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 	brake = Input.get_action_strength("break") * 200
-#	if Input.is_action_pressed("move_right"):
-#		direction.x += 1
-#	if Input.is_action_pressed("move_left"):
-#		direction.x -= 1
-#	if Input.is_action_pressed("move_backward"):
-#		direction.z += 1
-#	if Input.is_action_pressed("move_forward"):
-#		direction.z -= 1
-#
-#	direction.x += _x
-#	direction.z += _z
-#	_x = 0
-#	_z = 0
-#
-#	if direction != Vector3.ZERO:
-#		direction = direction.normalized()
-#		$Pivot.look_at(translation + direction, Vector3.UP)
-#
-#	velocity.x = direction.x * speed
-#	velocity.z = direction.z * speed
-#	velocity.y -= fall_acceleration * delta
-#	velocity = move_and_slide(velocity, Vector3.UP)
 
+# --------
+func _configure_collision():
+	set_contact_monitor(true)
+	set_max_contacts_reported(4)
+
+func _configure_distance_sensors():
+	for ray in DistanceSensors.get_children():
+		ray.set_enabled(true)
+		ray.set_exclude_parent_body(true)
+		ray.set_collide_with_bodies(true)
+		ray.set_collide_with_areas(false)
+		ray.set_cast_to(Vector3(0, -max_distance_sensor_proxemity, 0))
+
+func _configure_cameras():
+	var camera_storage = get_node(camera_storage_path)
+	for placeholder in CameraPlaceholders.get_children():
+		# TODO: remove hard-code with Camera keuword and use types
+		var camera = camera_storage.find_node(placeholder.name + "Camera")
+		placeholder.set_remote_node(camera.get_path())
+#
+
+# --------
+func get_proxemity() -> Dictionary:
+	# Return dictionary of distances from sensors
+	var proxemities = {}
+	for ray in DistanceSensors.get_children():
+		var proxemity: float = max_distance_sensor_proxemity
+		if ray.is_colliding():
+			proxemity = ray.global_translation.distance_to(ray.get_collision_point())
+		proxemities[ray.name] = proxemity 
+	return proxemities
+	
+func get_is_crashed() -> bool:
+	return is_collided
+
+func get_wheel_position() -> float:
+	return steering
+	
+func get_speed() -> float:
+	return $back_left_wheel.engine_force
+# --------
+func _on_Player_body_entered(body):
+	is_collided = true
 
