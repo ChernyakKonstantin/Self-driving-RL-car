c546bde5a491d8461a537c9df30f44cb3ec77cbf
KonstantinChernyak <chernyakonstantin@gmail.com>
2023-07-26 14:12:55 +0300

    Update training code with custom policy

--- training/train_to_steer_on_parking_sensors.py
+++ training/train_to_steer_on_parking_sensors.py
@@ -5,6 +5,8 @@ from stable_baselines3 import TD3, PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.noise import NormalActionNoise, VectorizedActionNoise
 import torch
+from models.parking_sensor_network import ParkingSensorExtractor, ParkingSensorNetwork
+from policies import CustomActorCriticPolicy
 
 from environments import GodotClient, TrainToSteerEnv
 
@@ -15,6 +17,7 @@ CHUNK_SIZE = 4096
 def train_to_steer_on_parking_sensors():
     env_fn = partial(TrainToSteerEnv, engine_client=GodotClient(engine_address=(ADDRESS, PORT), chunk_size=CHUNK_SIZE))
     env = make_vec_env(env_fn, n_envs=1)
+    custom_network_builder = partial(ParkingSensorNetwork)
     # action_noise = VectorizedActionNoise(NormalActionNoise(mean=np.array([0., ]), sigma=np.array([1, ])), n_envs=1)
     # model = TD3(
     #     "MultiInputPolicy",
@@ -30,12 +33,12 @@ def train_to_steer_on_parking_sensors():
     # TODO: try masacable ppo and disretized action to enable out-of-box masking
 
     policy_kwargs = {
-        "activation_fn": torch.nn.ReLU,
-        "net_arch": [64,64],
+        "custom_network_builder": custom_network_builder,
+        "features_extractor_class": ParkingSensorExtractor,
     }
 
     model = PPO(
-        "MultiInputPolicy",
+        CustomActorCriticPolicy,
         policy_kwargs=policy_kwargs,
         env=env,
         use_sde=False,
@@ -46,6 +49,9 @@ def train_to_steer_on_parking_sensors():
         tensorboard_log="/home/cherniak/Self-driving-RL-car/logs/train_to_steer_on_parking_sensors",
     )
     # model.set_parameters("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_PPO_1.zip")
-    model.learn(total_timesteps=150000)
+    model.learn(
+        total_timesteps=150000,
+        tb_log_name="parking_sensors_net_PPO",
+)
     # model.save("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_TD3_1.zip")
-    model.save("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_PPO_1.zip")
+    model.save("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_parking_sensors_net_PPO_1.zip")
