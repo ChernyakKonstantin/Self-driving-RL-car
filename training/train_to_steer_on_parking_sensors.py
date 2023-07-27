a04981ebb4d70636f3e1b4324fa1063330e47001
KonstantinChernyak <chernyakonstantin@gmail.com>
2023-07-27 18:45:13 +0300

    Update training with checkpointing

--- training/train_to_steer_on_parking_sensors.py
+++ training/train_to_steer_on_parking_sensors.py
@@ -1,19 +1,27 @@
+import os
 from functools import partial
 
 import numpy as np
-from stable_baselines3 import TD3, PPO
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.noise import NormalActionNoise, VectorizedActionNoise
 import torch
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.env_util import make_vec_env
+
+from environments import GodotClient, TrainToSteerEnv
 from models.parking_sensor_network import ParkingSensorExtractor, ParkingSensorNetwork
 from policies import CustomActorCriticPolicy
 
-from environments import GodotClient, TrainToSteerEnv
-
 ADDRESS = "localhost"
-PORT = 9090
+PORT = 9091
 CHUNK_SIZE = 4096
 PRETRAINED_ENCODER_WEIGHTS_PATTH = "/home/cherniak/Self-driving-RL-car/logs/pretrain_parking_sensor_encoder_with_icm/lightning_logs/version_0/checkpoints/epoch=49-step=3350.ckpt"
+LOG_DIR = "/home/cherniak/Self-driving-RL-car/logs/train_to_steer_on_parking_sensors/termination_on_collision"
+LOG_NAME = "parking_sensors_net_pretrained_with_icm_encoder_PPO"
+SUFFIX = "_1"
+TOTAL_TIMESTEPS = 150000
+N_STEPS = 4096
+CHECKPOINT_FREQUENCY = 5200
+LR = 1e-3
 
 def train_to_steer_on_parking_sensors():
     env_fn = partial(
@@ -24,18 +32,7 @@ def train_to_steer_on_parking_sensors():
     )
     env = make_vec_env(env_fn, n_envs=1)
     custom_network_builder = partial(ParkingSensorNetwork, pretrained_encoder_weights_path=PRETRAINED_ENCODER_WEIGHTS_PATTH)
-    # action_noise = VectorizedActionNoise(NormalActionNoise(mean=np.array([0., ]), sigma=np.array([1, ])), n_envs=1)
-    # model = TD3(
-    #     "MultiInputPolicy",
-    #     env,
-    #     learning_rate=1e-3,
-    #     learning_starts=2000,
-    #     action_noise=action_noise,
-    #     verbose=1,
-    #     device="cpu",
-    #     seed=0,
-    #     tensorboard_log="/home/cherniak/Self-driving-RL-car/logs/train_to_steer_on_parking_sensors",
-    # )
+
     # TODO: try masacable ppo and disretized action to enable out-of-box masking
 
     policy_kwargs = {
@@ -46,18 +43,23 @@ def train_to_steer_on_parking_sensors():
     model = PPO(
         CustomActorCriticPolicy,
         policy_kwargs=policy_kwargs,
+        n_steps=N_STEPS,
         env=env,
         use_sde=False,
-        learning_rate=1e-3,
+        learning_rate=LR,
         verbose=1,
         device="cpu",
         seed=0,
-        tensorboard_log="/home/cherniak/Self-driving-RL-car/logs/train_to_steer_on_parking_sensors",
+        tensorboard_log=LOG_DIR,
     )
     # model.set_parameters("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_PPO_1.zip")
     model.learn(
-        total_timesteps=150000,
-        tb_log_name="parking_sensors_net_termination_on_collision_pretrained_with_icm_encoder_PPO",
-)
-    # model.save("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_TD3_1.zip")
-    model.save("/home/cherniak/Self-driving-RL-car/train_to_steer_on_parking_sensors_net_termination_on_collision_pretrained_with_icm_encoder_PPO_1.zip")
+        callback=CheckpointCallback(
+            save_freq = CHECKPOINT_FREQUENCY,
+            save_path = os.path.join(LOG_DIR, LOG_NAME + SUFFIX, "checkpoints"),
+        ),
+        total_timesteps=TOTAL_TIMESTEPS,
+        tb_log_name=LOG_NAME,
+        progress_bar=True,
+    )
+    model.save(os.path.join(LOG_DIR, LOG_NAME + SUFFIX, "checkpoints", "last.zip"))
