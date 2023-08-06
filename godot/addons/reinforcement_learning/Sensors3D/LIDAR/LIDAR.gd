dfe70c2c21201657b9136287630dcd146f59ee8a
KonstantinChernyak <chernyakonstantin@gmail.com>
2023-08-06 18:05:08 +0300

    Move LIDAR and RGB camera to addons

--- godot/addons/reinforcement_learning/Sensors3D/LIDAR/LIDAR.gd
+++ godot/addons/reinforcement_learning/Sensors3D/LIDAR/LIDAR.gd
@@ -1,12 +1,29 @@
 # LIDAR orientation is along X-axis.
-
+#tool
 extends Spatial
 
-export var horizontal_resolution: float = 1 # Degrees
-export var vertical_resolution: float = 1 # Degrees
+export var horizontal_resolution: float = 2 # Degrees
+export var vertical_resolution: float = 2 # Degrees
 export var horizontal_fov: float = 120 # Degrees
-export var vertical_fov: float = 60 # Degrees
+export var vertical_fov: float = 30 # Degrees
 export var ray_max_len: float = 100 # Meters
+
+#func _debug_draw_lidar_range():
+#	var visual = ImmediateGeometry.new()
+#	visual.begin(Mesh.PRIMITIVE_TRIANGLES)
+#	visual.add_vertex(Vector3(0,0,0))
+#	visual.add_vertex(Vector3(0,1,1))
+#	visual.add_vertex(Vector3(0,-1,1))
+#	visual.end()
+#	var material = SpatialMaterial.new()
+#	material.albedo_color = Color(0.0, 0.0, 1.0) # Blue color
+#	visual.set_material_override(material)
+#	add_child(visual)
+	
+#
+#func _enter_tree():
+#	if Engine.is_editor_hint():
+#		_debug_draw_lidar_range()
 
 func _ready():
 	_create()
@@ -22,6 +39,7 @@ func _create() -> void:
 	for j in range(n_vertical_points):
 		for i in range(n_horizontal_points):
 			var ray = RayCast.new()
+			ray.set_visible(true)
 			ray.set_enabled(true)
 			ray.set_exclude_parent_body(true)
 			ray.set_collide_with_bodies(true)
